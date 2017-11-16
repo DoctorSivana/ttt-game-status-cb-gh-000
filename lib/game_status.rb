728bcdaf02f5608040a2f156fe101f101064e2e4
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 21:41:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,21 +4,48 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
-
 def won?(board)
-  WIN_COMBINATIONS.each do |item|
-    item.all? do |index|
-      index == "X" || index == "O"
-         return item
-         break
+  #display_board(board)
+  #returns false for an empty board
+  if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #puts "I am running"
+    return false
+  end
 
-       else
-         false
+  #returns false for draw
+  if board == ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+    return false
+  end
+
+  WIN_COMBINATIONS.each do |win_combination|
+    #puts "win_combination: #{win_combination}"
+    position_1 = board[win_combination[0]]
+    #puts "position_1: #{position_1}"
+    position_2 = board[win_combination[1]]
+    #puts "position_2: #{position_2}"
+    position_3 = board[win_combination[2]]
+    #puts "position_3: #{position_3}"
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      #puts "here is a winner #{win_combination}"
+      return win_combination
+      break
+    end
+  end
+  return false
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-end
+
 
 
 def full?(board)
@@ -32,22 +59,14 @@ def draw?(board)
 end
 
 def over?(board)
-  won?(board) || draw?(board) || full?(board)
-
+  won?(board) || full?(board) || draw?(board)
 end
 
 def winner(board)
-  if won?(board) = true
-    return board[won?(board)[0]]
+  if won?(board) != false
+        return board[won?(board)[0]]
   else
     return nil
   end
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
