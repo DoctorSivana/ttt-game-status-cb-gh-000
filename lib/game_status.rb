8321da2c152b94d222a178f4ac2cae73ebaec7c7
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 21:03:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,14 +5,14 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS {
-  [0,2,3],
+  [0,1,2],
   [3,4,5],
   [6,7,8],
   [0,3,6],
   [1,4,7],
-  [3,5,8],
+  [2,5,8],
   [0,4,8],
-  [3,4,6]
+  [2,4,6]
 }
 
 
