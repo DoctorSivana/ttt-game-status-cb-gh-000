39a82232c930fea1e69f0821018e98e5b131bcd7
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 21:37:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,16 +9,17 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
 
 def won?(board)
   WIN_COMBINATIONS.each do |item|
-    item.each do |index|
-      if board[index] == "X" || board[index] == "O"
+    item.all? do |index|
+      index == "X" || index == "O"
          return item
+         break
+         
        else
          false
-       end
-     end
-   end
-
 end
+end
+end
+
 
 def full?(board)
   !board.any? do |element|
